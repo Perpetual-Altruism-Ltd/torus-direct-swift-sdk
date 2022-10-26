ae1eb22cc34309f2142ef2aa77c724457f96af2f
Anil P <anil.prasad@tutanota.com>
2022-10-26 16:31:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -14,17 +13,16 @@ let package = Package(
             targets: ["CustomAuth"]),
     ],
     dependencies: [
-        .package(name:"TorusUtils", url: "https://github.com/torusresearch/torus-utils-swift.git",from: "3.0.0"),
+        .package(url: "https://github.com/Perpetual-Altruism-Ltd/torus-utils-swift.git", branch: "master"),
         .package(name:"jwt-kit", url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
         .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift.git", from: "2.6.3")
     ],
     targets: [
         .target(
             name: "CustomAuth",
-            dependencies: ["TorusUtils","JWTDecode"]),
+            dependencies: [.product(name: "TorusUtils", package: "torus-utils-swift"), "JWTDecode"]),
         .testTarget(
             name: "CustomAuthTests",
-            dependencies: ["CustomAuth", .product(name: "JWTKit", package: "jwt-kit")]),
+            dependencies: ["CustomAuth", .product(name: "TorusUtils", package: "torus-utils-swift"), .product(name: "JWTKit", package: "jwt-kit")]),
     ]
-    ,swiftLanguageVersions: [.v5]
 )
